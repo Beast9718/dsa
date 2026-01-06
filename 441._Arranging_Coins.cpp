1f4180d2c80a0e22eddfffc0e8bd66fbbe00585d
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-01-06 20:40:23 +0530

    441. Arranging Coins

---
+++ 441._Arranging_Coins.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ int arrangeCoins(int n) {
+        long long x=0;
+        long long temp=0;
+        for(int i=1;i<9;i++){
+            temp+=n;
+        }
+        x+=(-1+sqrt(1+temp))/2;
+        return x;
+         
+    }
+
+int main()
+{
+    int n=7;
+    int result=arrangeCoins(n);
+    cout<<result;
+
+
+}
