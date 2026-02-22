8027555fbbda126ba9d9231e7c101464164fd36d
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-02-22 22:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (21.99%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int cnt=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]%2==1){
+                cnt+=1;
+            }
+            if(cnt==3){
+                break;
+                
+            }
+            if (arr[i]%2==0){
+                cnt=0;
+            }
+        }
+        if(cnt==3)return true;
+        else return false;
+    }
+};
