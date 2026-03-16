709d2b6882496672b3b3274183d088c582ab9320
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-03-17 00:25:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (34.30%) - LeetSync

--- 3540-hash-divided-string/hash-divided-string.cpp
+++ 3540-hash-divided-string/hash-divided-string.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     string stringHash(string s, int k) {
-        string result="";
-        int size=s.length();
-        int sum=0;
-        int n=0;
-        for(int i=0;i<size/k;i++){
-            sum=0;
-            
-            for(int j=0;j<k;j++){
-             sum+=s[n]-'a';
-             n++;
-                
+        string result = "";
+        int size = s.length();
+        int sum = 0;
+        int n = 0;
+        for (int i = 0; i < size / k; i++) {
+            sum = 0;
+
+            for (int j = 0; j < k; j++) {
+                sum += s[n] - 'a';
+                n++;
             }
-                 result+=char(97+(sum%26));
+            result += char(97 + (sum % 26));
         }
         return result;
     }
