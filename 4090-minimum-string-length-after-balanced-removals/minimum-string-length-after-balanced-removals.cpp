71c9b01bcbed6b3478be184479fa629b7f98e4cf
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-03-23 22:00:30 +0530

    Time: 6 ms (35.85%) | Memory: 14.9 MB (70.73%) - LeetSync

--- 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int minLengthAfterRemovals(string s) {
-        int output=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='a') output+=1;
-            if(s[i]=='b') output-=1;
+        int output = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a')
+                output += 1;
+            if (s[i] == 'b')
+                output -= 1;
         }
-     if(output>0)return output;
-     else return -output;
+        if (output > 0)
+            return output;
+        else
+            return -output;
     }
 };
