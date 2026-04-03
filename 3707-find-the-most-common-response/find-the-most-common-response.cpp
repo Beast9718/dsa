cee48702062d224646f44896c1de822ac3cf2840
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-04-03 23:24:26 +0530

    Time: 1223 ms (57.11%) | Memory: 488.7 MB (41.88%) - LeetSync

---
+++ 3707-find-the-most-common-response/find-the-most-common-response.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string findCommonResponse(vector<vector<string>>& responses) {
+        unordered_map<string,int>map;
+        for(int i=0;i<responses.size();i++){
+         
+                set<string>s(responses[i].begin(),responses[i].end());
+                for(auto str:s){
+                            map[str]++;
+                }
+            
+        }
+        string str="";
+        int max_cnt=0;
+        for(auto it:map){
+            if(it.second>max_cnt){
+                str=it.first;
+                max_cnt=it.second;
+            }
+              else if (it.second == max_cnt && it.first < str) {
+        str = it.first;
+    }
+            
+        }
+        return str;
+    }
+};
