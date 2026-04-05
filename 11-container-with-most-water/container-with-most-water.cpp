08455646121260bf62bd790b03d27ebe93176550
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-04-05 21:41:08 +0530

    Time: 3 ms (35.68%) | Memory: 62.9 MB (47.79%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -3,14 +3,15 @@ public:
     int maxArea(vector<int>& height) {
         int l = 0;
         int r = height.size() - 1;
-        vector<int> ans;
+        int ans=0;
         while (l < r) {
-            ans.push_back(min(height[l], height[r]) * (r - l));
+           int area=(min(height[l], height[r]) * (r - l));
+           ans=max(ans,area);
             if (height[l] < height[r])
                 l++;
             else
                 r--;
         }
-        return *max_element(ans.begin(), ans.end());
+        return ans;
     }
 };
