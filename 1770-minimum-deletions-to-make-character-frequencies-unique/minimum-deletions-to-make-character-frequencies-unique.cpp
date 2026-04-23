4e2b41278853aabf7cb21b160accf0c1ff386ae9
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-04-23 18:18:26 +0530

    Time: 18 ms (31.20%) | Memory: 20.6 MB (28.59%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char,int>freq;
-        for(char c:s){
+        unordered_map<char, int> freq;
+        for (char c : s) {
             freq[c]++;
         }
-        int ans=0;
-        unordered_set<int>used;
-        for(auto &[ch,cnt]:freq){
-            while(cnt>0 && used.count(cnt)){
+        int ans = 0;
+        unordered_set<int> used;
+        for (auto& [ch, cnt] : freq) {
+            while (cnt > 0 && used.count(cnt)) {
                 cnt--;
                 ans++;
             }
-            if(cnt>0)used.insert(cnt);
+            if (cnt > 0)
+                used.insert(cnt);
         }
         return ans;
-        
     }
 };
