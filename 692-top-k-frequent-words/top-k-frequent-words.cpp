0025dca158534a16e0d83edcef7c5400bb844007
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-04-23 17:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (93.21%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int>freq;
+        for(string s:words){
+            freq[s]++;
+        }
+        vector<pair<string,int>>v(freq.begin(),freq.end());
+        sort(v.begin(),v.end(),[](auto &a,auto &b){if(a.second==b.second)return a.first<b.first;
+        return a.second>b.second;});
+        vector<string>output;
+        for(int i=0;i<k;i++){
+            output.push_back(v[i].first);
+        }
+        return output;
+
+    }
+};
