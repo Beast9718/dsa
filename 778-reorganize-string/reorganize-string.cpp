6af4932732a6d029c26df25a0499364475a7289d
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-04-24 18:26:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.07%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char,int> freq;
-        for(char x:s){
+        unordered_map<char, int> freq;
+        for (char x : s) {
             freq[x]++;
         }
-        priority_queue<pair<int,char>>pq;
-        for(auto &[ch,cnt]:freq){
-            pq.push({cnt,ch});
+        priority_queue<pair<int, char>> pq;
+        for (auto& [ch, cnt] : freq) {
+            pq.push({cnt, ch});
         }
-string ans="";
+        string ans = "";
 
-        while(pq.size()>1){
-            auto [cnt1,ch1]=pq.top();
-             ans+=ch1;
-             cnt1--;
+        while (pq.size() > 1) {
+            auto [cnt1, ch1] = pq.top();
+            ans += ch1;
+            cnt1--;
             pq.pop();
-            auto [cnt2,ch2]=pq.top();
-            ans+=ch2;
+            auto [cnt2, ch2] = pq.top();
+            ans += ch2;
             cnt2--;
             pq.pop();
 
-            if(cnt1>0)pq.push({cnt1,ch1});
-            if(cnt2>0)pq.push({cnt2,ch2});
+            if (cnt1 > 0)
+                pq.push({cnt1, ch1});
+            if (cnt2 > 0)
+                pq.push({cnt2, ch2});
         }
-        if(pq.size()!=0){
-            auto [cnt,ch]=pq.top();
-            if(cnt>1)return "";
-            ans+=ch;
-            
+        if (pq.size() != 0) {
+            auto [cnt, ch] = pq.top();
+            if (cnt > 1)
+                return "";
+            ans += ch;
         }
         return ans;
     }
