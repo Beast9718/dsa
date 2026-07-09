92cc71d112424f3115c5a81a069f9839fc907a5b
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-07-09 10:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (18.88%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> powerset;
+    void generate(vector<int>& subset, int i, vector<int>& nums) {
+        if (i == nums.size()) {
+            powerset.push_back(subset);
+            return;
+        }
+
+        generate(subset, i + 1, nums);
+
+        subset.push_back(nums[i]);
+        generate(subset, i + 1, nums);
+        subset.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subset;
+        int i = 0;
+        generate(subset, i, nums);
+        return powerset;
+    }
+};
