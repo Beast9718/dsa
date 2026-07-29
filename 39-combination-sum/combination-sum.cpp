c4c8ed2c18e94ebdd7f4dfa7970c43d3768878d5
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-07-29 08:19:08 +0530

    Time: 47 ms (12.19%) | Memory: 45.3 MB (9.96%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void FindCombination(int ind,int target,vector<int>& candidates,vector<vector<int>>&ans,vector<int>ds){
-        if(ind==candidates.size()){
-            if(target==0){ans.push_back(ds);}
+    void FindCombination(int ind, int target, vector<int>& candidates,
+                         vector<vector<int>>& ans, vector<int> ds) {
+        if (ind == candidates.size()) {
+            if (target == 0) {
+                ans.push_back(ds);
+            }
             return;
-
         }
-        
-        if(candidates[ind]<=target){
+
+        if (candidates[ind] <= target) {
             ds.push_back(candidates[ind]);
-            FindCombination(ind,target-candidates[ind],candidates,ans,ds);
+            FindCombination(ind, target - candidates[ind], candidates, ans, ds);
             ds.pop_back();
         }
-        FindCombination(ind+1,target,candidates,ans,ds);
+        FindCombination(ind + 1, target, candidates, ans, ds);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int>ds;
-        FindCombination(0,target,candidates,ans,ds);
+        vector<vector<int>> ans;
+        vector<int> ds;
+        FindCombination(0, target, candidates, ans, ds);
         return ans;
     }
 };
